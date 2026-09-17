bcf9c15d68ebc9b96f959f93f94e535091dc9c56
mingyo77 <wells7845@naver.com>
2026-09-17 21:16:31 +0900

    feat: add focus mode control

--- src/Altong.Client/App.xaml.cs
+++ src/Altong.Client/App.xaml.cs
@@ -1,7 +1,9 @@
 using System.Windows;
+using Altong.Client.Services;
 
 namespace Altong.Client;
 
 public partial class App : Application
 {
+    public FocusModeService FocusModeService { get; } = new();
 }
